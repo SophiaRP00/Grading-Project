df16e768e6e8b72fe9fd69a223dec2bdb8ef4ae4
Adam <adam.bhh@live.com>
2022-11-28 11:35:21 +0100

    made functions

---
+++ grading.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def roundGrade(grades):
+    return 0
+
+def computeFinalGrades(grades):
+    return 0
+def gradesPlot(grades):
+    return 0
+
+def main():
+    print("hey")
+main()
